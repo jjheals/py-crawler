86de68ebbc520c3ca793a80cd64304ecb3d15785
Justin Healey <jjheals@gmail.com>
2023-10-08 20:26:52 -0400

    Completed basic crawler capabilities

---
+++ Crawler.py
@@ -0,0 +1,124 @@
+import requests 
+from bs4 import BeautifulSoup
+import re
+import nltk
+import random as rand
+from URLIndex import URLIndex
+
+class Crawler: 
+    
+    base_url:str
+    url_index:URLIndex
+    tele_factor:int = 0.1
+    
+    def __init__(self, base_url:str):
+        self.base_url = base_url
+        self.url_index = URLIndex()
+        
+    @staticmethod
+    def __process_text__(text:str) -> list[str]: 
+        # Remove any "\"s to avoid errors with parsing the raw content 
+        text = re.sub(r'\\', '', text) 
+        
+        processed_tokens:str = []     # Processed text as tokens to return 
+
+        # Install the required packages from NLTK if needed 
+        try: nltk.corpus.wordnet
+        except LookupError: nltk.download('wordnet')
+        
+        try: nltk.data.find('tokenizers/punkt') 
+        except LookupError: nltk.download('punkt')
+        
+        try: nltk.corpus.stopwords.words('english')
+        except LookupError: nltk.download('stopwords') 
+
+        # Init BS parser 
+        soup = BeautifulSoup(text, 'html.parser')
+
+        # Perform tokenization 
+        # Replace any special chars in the content with spaces to act as delimeters 
+        pattern:str = r'[^a-zA-Z0-9\s]'     # Pattern of plaintext characters (a-z, A-Z, 0-9, no special chars)
+        text = re.sub(pattern, ' ', text)      # Substitute all matches with spaces  
+            
+        # Split the text on spaces, convert to lower, and strip whitespace from each token 
+        tokens = [s.lower().strip() for s in text.split(' ') if not s.isspace()]
+            
+        # Perform stemming
+        # DO SOMETHING 
+        
+        # Remove stop words 
+        # DO SOMETHING ... 
+        
+        return tokens    
+            
+    @staticmethod
+    def __fetch__(url:str) -> str:
+        headers = { 
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'              
+        }
+        
+        try: 
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            return response.content
+        except requests.exceptions.RequestException as e:
+            print(e)
+            return f"ERROR: RequestException in Crawler.__fetch__() for URL \"{url}\"."
+        except Exception as e:
+            print(e)
+            return f"ERROR: Unknown exception in Crawler.__fetch__() for URL \"{url}\"."
+    
+    
+    def index_url(self, url:str):
+
+        # Since this function is recursive, check if we've seen this URL before and simply return if we have
+        if self.url_index.check_url_exists(url): return 
+        
+        # Keep track of the number of original URLs so we know how many new ones we visited 
+        original_num_urls = len(self.url_index.urls)
+            
+        try:
+            # Fetch the content of the base URL
+            content:str = Crawler.__fetch__(url)
+            soup = BeautifulSoup(content, 'html.parser')    # Parse the HTML
+            
+            # Find links in the content
+            hyperlinks = [f"{'/'.join(url.split('/')[:-1])}/{link.get('href')}" for link in soup.find_all('a')]
+            
+            # Tokenize the content 
+            tokens:list[str] = Crawler.__process_text__(soup.text)
+            
+            # Add the new URL to the url_index
+            if not self.url_index.new_url(url, len(tokens), tokens, hyperlinks): 
+                print(f"new_url({url}) == False")
+                return
+            
+            # Use the teleportation factor to determine if we're going to follow the hyperlinks or not 
+            teleport:bool = (rand.randint(0,1) <= self.tele_factor)
+            if teleport: 
+                # Jump to a random page from hyperlinks (with equal probability)
+                link_index:int = rand.randint(0, len(hyperlinks)-1)
+                next_link:str = hyperlinks[link_index]
+                hyperlinks.remove(next_link)
+                            
+                # Use recursion to visit the next link 
+                self.index_url(next_link)
+            
+            # NOTE: run this WITHOUT "else" condition so that we hit all hyperlinks on the page eventually
+            # Visit the next hyperlink in the list of hyperlinks (assuming we haven't visited it yet)
+            while hyperlinks:
+                next_link:str = hyperlinks[0]
+                hyperlinks.remove(next_link)                
+                self.index_url(next_link)
+            
+            # Calculate the difference between the original number of urls and the current number of urls to calc
+            # how many urls we visited in total
+            return len(self.url_index.urls) - original_num_urls
+        
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching the content for {url}: {e}")
+            return len(self.url_index.urls) - original_num_urls
+        except Exception as e:
+            print(f"Error: {e}")
+            return len(self.url_index.urls) - original_num_urls
+        
---
+++ URLIndex.py
@@ -0,0 +1,125 @@
+
+class URLIndex: 
+    
+    ''' index:dict[str, dict[int,int]] - Index of the terms and their frequencies (key:val == term:[{url_id,frequency}])
+        Ex: {
+            "auto": {
+                      1:1, 
+                      5:2, 
+                      10:21
+                    },
+            "games": { 
+                      3:4,
+                      38:4,
+                      24: 12
+                    },
+            ... : { ... }
+    '''
+    index:dict[str, list[dict[int,int]]] 
+    
+    ''' url_num_terms:dict[int, int] - Number of terms in each URL to calc page rank (key:val == url_id,num_terms)
+        Ex: {
+              0 : 496,
+              1 : 156,
+              2 : 52, 
+              3 : 845, 
+              ... : ... 
+              len(self.urls) - 1 : num_terms
+            }
+    '''
+    url_num_terms:dict[int, int]        
+    
+    ''' url_hyperlinks:dict[int, list[str]] - Hyperlinks seen in each URL (key:val == url_id:[hyperlinks])
+        Ex: { 
+              0: ["https://google.com", 
+                  "https://facebook.com" 
+                 ],
+              1: ["https://reddit.com",
+                  "https://cs.wpi.edu/cs2192"
+                 ],
+              2: [],
+              ... : ... 
+              len(self.urls) - 1: ["https://link1.net/endpoint1/endpoint2",
+                                   "https://link2.edu" 
+                                  ]    
+            }
+    ''' 
+    url_hyperlinks:dict[int, list[str]] 
+    
+    ''' urls:list[str] - List of all seen URLs 
+        Ex: ["https://google.com", 
+            "https://facebook.com", 
+            "https://reddit.com/web-crawling",
+            ... 
+            ]
+    '''
+    urls:list[str]  
+    
+    def __init__(self): 
+        
+        # Check if the given base URL is a valid URL
+        # NOTE: use regex, throw error if not
+        # DO SOMETHING ... 
+        
+        self.index = {}     
+        self.url_num_terms = {}
+        self.url_hyperlinks = {}
+        self.urls = []  
+
+    ''' check_url_exists(url) - check if the URL exists in the index
+        
+        PARAMETERS: 
+            url (str) - URL to check if exists
+            
+        RETURNS: 
+            True if the URL DOES exist, False if the URL DOES NOT exist
+    '''
+    def check_url_exists(self, url:str) -> bool: 
+        return url in self.urls
+    
+    ''' new_url(url) - add a new URL to the index, return FALSE if the url exists already
+        
+        PARAMETERS: 
+            url (str) - new URL to add
+            
+        RETURNS: 
+            True if added successfully, false if the url exists already        
+    '''
+    def new_url(self, url:str, num_terms:int, tokenized_content:list[str], hyperlinks:list[str]) -> bool:
+        
+        # Check if we've seen this URL yet, return False if we have
+        if self.check_url_exists(url): 
+            print(f"check_url_exists({url}) == True") 
+            return False
+        
+        # Get the will-be ID for this URL 
+        # NOTE: called before appending b/c self.urls is 0-indexed, so the id is len(self.urls) - 1 AFTER new url is added,
+        #       thus is just the length BEFORE new url is added
+        this_id:int = len(self.urls) 
+        
+        # Adding the data from the new URL
+        self.urls.append(url)                       # Add the url to self.urls
+        self.url_num_terms[this_id] = num_terms     # Save the number of terms 
+        self.url_hyperlinks[this_id] = hyperlinks   # Save the hyperlinks
+        
+        # Add the tokenized content for this url to self.index
+        for t in tokenized_content: 
+            # If the token does not already exist in the index, create a new key with a dictionary containing only this url_id and "1" for the term freq
+            if not t in self.index: self.index[t] = {this_id: 1} 
+                
+            # If the token DOES already exist in the index 
+            else: 
+                curr_val = self.index[t]  # Get the current value (url_ids and term freq) for this token in self.index
+                
+                # Check if this url_id already exists in the dict for this token
+                # If this url_id DOES exist already, add 1 to the term frequency 
+                if this_id in curr_val: curr_val[this_id] = curr_val[this_id] + 1   
+                
+                # If this url_id does NOT exist already, then add it with "1" as the term frequency
+                else: curr_val[this_id] = 1                      
+                
+                # Replace this value in self.index with the updated value
+                self.index[t] = curr_val    
+        
+        return True
+        
